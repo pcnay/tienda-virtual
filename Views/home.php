76c7a63552c4b7491592d50b0b966fe0d5b9d87e
Ramon <pcnay2003@gmail.com>
2022-12-30 22:50:27 -0800

    Se muestra en pantalla las categorias (solo la vista) aun no se extrae desde la tabla de Categorias

--- Views/home.php
+++ Views/home.php
@@ -64,7 +64,7 @@
 						<div class="block1 wrap-pic-w">
 							<img src="<?= $arrBanner[$j]['portada'] ?>" alt="$arrBanner[$j]['nombre']">
 
-							<a href="<?= base_url().'/tienda/categoria/'.$arrBanner[$j]['nombre']; ?>" class="block1-txt ab-t-l s-full flex-col-l-sb p-lr-38 p-tb-34 trans-03 respon3">
+							<a href="<?= base_url().'/Tienda/Categoria/'.$arrBanner[$j]['nombre']; ?>" class="block1-txt ab-t-l s-full flex-col-l-sb p-lr-38 p-tb-34 trans-03 respon3">
 								<div class="block1-txt-child1 flex-col-l">
 									<span class="block1-name ltext-102 trans-04 p-b-8">
 										<?php echo $arrBanner[$j]['nombre']; ?>
